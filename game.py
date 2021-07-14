1812796eada0300a4db1c0748b445042c6ccf057
Kortney Foley <kortneyfoley@Kortneys-MacBook-Pro.local>
2021-07-14 20:46:10 -0300

    added first character choice

--- game.py
+++ game.py
@@ -10,3 +10,10 @@ characterName = ""
 characterName = input("choose a character name: ")
 print("You have chosen the name " + characterName) 
 
+playerChoiceOne = input("You come to a fork in the road. Choose left or right: ")
+if(playerChoiceOne.lower() == "left"):
+    print("You have died")
+elif(playerChoiceOne.lower() == "right"):
+    print("you have won")
+else: 
+    print("that is not a vaild option")
