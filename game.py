a5a1f42cdd5018165dfc4d9d3441785f6a094fa1
Kortney Foley <kortneyfoley@Kortneys-MacBook-Pro.local>
2021-07-14 20:33:29 -0300

    created game title with art

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
+
+gameTitle = "◥(ฅº￦ºฅ)◤        Spooky.Game        ⊂(•̀﹏•́⊂ )∘˚˳°"
+print("=========================================================")
+print("")
+print(gameTitle)
+print("")
+print("=========================================================")
+
 characterName = ""
 characterName = input("choose a character name: ")
 print("You have chosen the name " + characterName) 
