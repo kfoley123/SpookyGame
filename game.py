1004fa1ba6f0e26435e2fc070b301a7b494bd255
Kortney Foley <kortneyfoley@Kortneys-MacBook-Pro.local>
2021-07-26 21:50:57 -0300

    added second player choice and made it loop if its an invalid choice

--- game.py
+++ game.py
@@ -21,7 +21,26 @@ while lives > 0:
         lives -= 1
         print("remaining lives: "+ str(lives))
     elif(playerChoiceOne.lower() == "right"):
-        print(characterName + " has won")
+    
+        invalidinput = True
+        while(invalidinput):
+            playerChoiceTwo = input("You see a chest on the side of the path. Do you open it? ")
+            if(playerChoiceTwo.lower() == "yes" or playerChoiceTwo.lower() == "yeah" or playerChoiceTwo.lower() == "y"):
+                print("The chest is full of fangs that bite down on your arm, servering it. You bleed to death!")
+                print("")
+                print(characterName + " has died")
+                print("⎧ᴿᴵᴾ⎫◟◟◟◟◟◟◟◟ ❀◟(ó ̯ ò, )")
+                print("")
+                lives -= 1
+                print("remaining lives: "+ str(lives))
+                invalidinput = False
+            elif(playerChoiceTwo.lower() == "no" or playerChoiceTwo.lower() == "nope" or playerChoiceTwo.lower() == "n"):
+                print(characterName + " noticed that the chest is a mimic and would have eaten you. Great job! You keep walking down the path ")
+                invalidinput = False
+            else: 
+                print("that is not a valid option")
+
+
     else: 
         print("that is not a vaild option")
 
